b4ecce634cfc22d0300b066453f01aced6c7d7e8
Patrick Meaney <patrick.wm.meaney@gmail.com>
2023-12-08 12:34:27 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+
+###  Terraform Env Vars
+
+
+```bash
+# Example of pulling an item from 1pass and setting it into an env var.
+export TESTITEM=$(op item get "ClicksAndCodes 1A server" --fields label=testItem)
+```
+
+```bash
+# tf only picks up env vars that are prefixed with TF_VAR_
+# The DigitalOcean token is not used within the TF script so it does not need the TF_VAR_ prefix.
+export DIGITALOCEAN_ACCESS_TOKEN=$(op item get "ClicksAndCodes 2A server" --fields label=DO_TOKEN_CCC_120823_2A)
+
+# Note that this set of tf env vars has `&&` connecting them, so copy & paste them as a block.
+export TF_VAR_LINUX_USER_DEVOPS=$(op item get "ClicksAndCodes 2A server" --fields label=LINUX_USER_DEVOPS_2A) &&
+export TF_VAR_LINUX_SSH_KEY=$(op item get "ClicksAndCodes 2A server" --fields label=LINUX_SSH_KEY_2A) &&
+export TF_VAR_LINUX_SERVER_NAME=$(op item get "ClicksAndCodes 2A server" --fields label=LINUX_SERVER_NAME_2A)
+
+```
+
+
+For this project, we will use all new env vars, secrets, keys, etc.
+
+- add ssh key
+  - to generate new ssh key: `ssh-keygen -t ed25519 -C "emailAddressHere"`
+  - Add the .pub key to 1pass. (example key name: LINUX_SSH_KEY_2A, with value of your ssh key)
+  - Add the .pub key to digitalocean.
+  - (Later, if you use CICD you'll need to add the private key to its secrets or in some way get the private key can be upload to the remote server it generates)
+- Generate a Digital Ocean API key & add it to 1pass (example key name: DO_TOKEN_CCC_120823_2A, with value of your DO API Key)
+- Pick server name & add it to 1pass (example key name: LINUX_SERVER_NAME_2A, with value of your chosen server name)
+- Pick server user's name & add it to 1pass (example key name: LINUX_USER_DEVOPS_2A, with value of your chosen server user name)
+
+
---
+++ terraform-with-cloudinit/tf-dev.tf
@@ -0,0 +1,81 @@
+# File: v1-tf-gha-test1a.tf
+
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+locals {
+  sizes = {
+    nano      = "s-1vcpu-1gb"
+  }
+  regions = {
+    san_francisco = "sfo3"
+  }
+}
+
+provider "digitalocean" {}
+
+# the SERVER_NAME is not as important to set via env var... but we will go ahead and do it
+variable "LINUX_SERVER_NAME" {
+  type = string
+  description = "environment variable for devops user"
+  default = "blahServerName"
+}
+
+variable "LINUX_USER_DEVOPS" {
+  type = string
+  description = "environment variable for devops user"
+  default = "blahLinxUser"
+}
+
+variable "LINUX_SSH_KEY" {
+  type = string
+  description = "environment variable for devops ssh key"
+  default = "blahSshKey"
+}
+
+data "template_file" "my_example_user_data" {
+  template = templatefile("./yamlScripts/v1-cloudinit-gha-test1a.yaml",
+    {
+      LINUX_USER_DEVOPS = "${var.LINUX_USER_DEVOPS}",
+      LINUX_SSH_KEY = "${var.LINUX_SSH_KEY}",
+    })
+}
+
+resource "digitalocean_droplet" "droplet" {
+  image     = "ubuntu-22-04-x64"
+  name      = "${var.LINUX_SERVER_NAME}"
+  region    = local.regions.san_francisco
+  size      = local.sizes.nano
+  tags      = ["terraform", "docker"]
+  user_data = data.template_file.my_example_user_data.rendered
+}
+
+output "ip_address" {
+  value       = digitalocean_droplet.droplet.ipv4_address
+  description = "The public IP address of your droplet."
+}
+
+
+output "template_file_contents" {
+  value = data.template_file.my_example_user_data.rendered
+}
+
+output "LINUX_SERVER_NAME" {
+  value = "${var.LINUX_SERVER_NAME}"
+}
+
+output "LINUX_USER_DEVOPS" {
+  value = "${var.LINUX_USER_DEVOPS}"
+}
+
+output "LINUX_SSH_KEY" {
+  value = "${var.LINUX_SSH_KEY}"
+}
